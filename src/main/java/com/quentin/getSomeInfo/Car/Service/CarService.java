7a4f403a8a334b5e27f1a8f7e06fe6bfd678bc9c
Quentin Berger <quentin.berger@zalando.de>
2023-11-03 13:17:25 +0100

    Add optional for car (not prouction smart, but for the sake of testin

--- src/main/java/com/quentin/getSomeInfo/Car/Service/CarService.java
+++ src/main/java/com/quentin/getSomeInfo/Car/Service/CarService.java
@@ -1,6 +1,9 @@
 package com.quentin.getSomeInfo.Car.Service;
 
 import java.util.ArrayList;
+import java.util.Optional;
+
+import javax.swing.text.html.Option;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +23,14 @@ public class CarService implements CarApi{
         cars.add(new Car("BMW", "i8", 128999.00, 3, FUEL.ELECTRIC, "Blue", TRANSMISSION.AUTOMATIC));
         cars.add(new Car("Mazda", "626", 2000.99, 5, FUEL.DIESEL, "Green", TRANSMISSION.AUTOMATIC));
         cars.add(new Car("Hyundai", "i10", 16999.99, 3, FUEL.HYBRID, "White", TRANSMISSION.HALFAUTOMATIC));
+        cars.add(createCarWithDefaultColor("Mazda", "626", 2000.99, 5, Optional.of("Green")));
+        cars.add(createCarWithDefaultColor("Mazda", "626", 2000.99, 5, Optional.empty()));
         return cars;
+    }
+
+    public Car createCarWithDefaultColor(String brand, String model, Double price, Integer doorCount, Optional<String> color) {
+        String optionalColor = color.orElse("white");
+        return new Car(brand, model, price,doorCount, FUEL.PETROL, optionalColor, TRANSMISSION.AUTOMATIC);
     }
     
 }
